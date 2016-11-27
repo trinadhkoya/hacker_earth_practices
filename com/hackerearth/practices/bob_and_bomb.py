84601433b8faf41d5ea829173e01d413affe22eb
trinadhkoya <trinadhkoya9@gmail.com>
2016-11-27 21:45:40 +0530

    bob and bombs

--- com/hackerearth/practices/bob_and_bomb.py
+++ com/hackerearth/practices/bob_and_bomb.py
@@ -3,18 +3,45 @@ Created on 23-Nov-2016
 
 @author: trinadhkoya
 '''
+
+
+def check_count(str):
+    k=0
+    str=str
+    str="--{}--".format(str)
+    str=list(str)
+    for i,j in enumerate(str):
+        if str[i] == 'B':
+            if str[i-1] == 'W':
+                k += 1
+                str[i-1] = '-'
+            if str[i-2] == 'W':
+                k += 1
+                str[i-2] = '-'
+            if str[i+1] == 'W':
+                k += 1
+                str[i+1] = '-'
+            if str[i+2] == 'W':
+                k += 1
+                str[i+2] = '-'
+    print(k)
+
 T=int(input())
-
-
-
-def check_walls_count(input, T):
-    counter=0
-    
-    for i in range(len(input)):
-        if input[i]=='B':
-            
-            
-            
 while T!=0:
-    result=check_walls_count(str(input()),T)
-print(result)
+    check_count(str(input()))
+    T-=1
+    
+    
+    
+    
+                
+
+                
+    
+            
+            
+    
+    
+       
+    
+            
