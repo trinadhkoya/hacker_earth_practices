ebd58c3fcad1e9900ec3f44c372a774eb20b66cd
androidmates <trinadhkoya@outlook.com>
2016-11-30 16:41:48 +0530

    changes made to print hackerearth:

--- com/hackerearth/practices/print_hacker_earth.py
+++ com/hackerearth/practices/print_hacker_earth.py
@@ -5,5 +5,16 @@ Created on Nov 30, 2016
 '''
 
 N=int(input())
-input_=list(map(str,input().lower()))[:N]
-print (min((input_.count("h")//2, input_.count("a")//2, input_.count("c"), input_.count("k"),input_.count("e")//2,input_.count("r")//2, input_.count("t"))))
+
+if 1<=N<=pow(10,6):
+    Str=list(map(str,input().lower()))[:N]
+    Str_h_count=Str.count("h")//2;
+    Str_a_count=Str.count("a")//2;
+    Str_c_count=Str.count("c");
+    Str_k_count=Str.count("k");
+    Str_e_count=Str.count("e")//2;
+    Str_r_count=Str.count("r")//2;
+    Str_t_count=Str.count("t");
+    
+    print(min(Str_h_count,Str_a_count,Str_c_count,Str_k_count,Str_e_count,Str_r_count,Str_t_count))
+
